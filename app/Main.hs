8477e45ff3086113abe8e9400ea79a5ea049531f
Dane Bulat <dane.bulat@gmail.com>
2022-08-31 03:52:27 +0100

    web socket connection boilerplate

--- app/Main.hs
+++ app/Main.hs
@@ -2,10 +2,32 @@
 
 module Main where
 
+import Control.Concurrent (forkIO)
+import Control.Monad (forever, unless, void)
 import Data.Aeson (encode, decode)
+import Data.Text (Text, pack)
 import qualified Data.ByteString.Lazy as B
+
+import Network.WebSockets (ClientApp, receiveData, sendClose, sendTextData, sendPing)
+import qualified Wuss 
 import WebTypes
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = Wuss.runSecureClient "echo.websocket.events" 443 "/" ws
 
+ws :: ClientApp ()
+ws connection = do
+  putStrLn "Connected!"
+
+  void . forkIO . forever $ do
+    message <- receiveData connection
+    print (message :: Text)
+
+  loop
+  sendClose connection (pack "Bye!")
+  where
+    loop = do
+      line <- getLine
+      unless (null line) $ do
+        sendTextData connection (pack line)
+        loop
