43d0c7ad00da5c37b99419969f9afcd9c78341ea
Dane Bulat <dane.bulat@gmail.com>
2022-08-31 05:02:10 +0100

    stream requested symbol's mini ticker

--- app/Main.hs
+++ app/Main.hs
@@ -2,11 +2,13 @@
 
 module Main where
 
-import Control.Concurrent (forkIO, ThreadId, throwTo)
+import Control.Concurrent (forkIO, ThreadId, throwTo, threadDelay)
 import Control.Monad (forever, unless, void, forM_)
 import Data.Aeson (encode, decode)
 import Data.Text (Text, pack)
+import qualified Data.Text as T
 import qualified Data.ByteString.Lazy as B
+import qualified System.Random as R
 
 import qualified Network.WebSockets as WS
 import Network.WebSockets (ClientApp, receiveData, sendClose, sendTextData, sendPing)
@@ -14,12 +16,18 @@ import qualified Wuss
 import WebTypes
 import Control.Exception (AsyncException(ThreadKilled))
 
-main :: IO ()
-main = Wuss.runSecureClient "echo.websocket.events" 443 "/" ws
+data BinanceConn = BinanceConn
+  { url  :: Text
+  , port :: Int
+  , path :: Text
+  }
+
+-- -------------------------------------------------------------------
+-- Web Sockets
 
 -- | App within web socket connection context 
-ws :: ClientApp ()
-ws conn = do
+wsApp :: ClientApp ()
+wsApp conn = do
   putStrLn "Connected!"
 
   t1 <- receiveWs conn
@@ -31,10 +39,13 @@ ws conn = do
 -- | Loop for echoing
 sendWs :: WS.Connection -> IO ()
 sendWs conn = do
-  line <- getLine
-  unless (null line) $ do
-    sendTextData conn (pack line)
-    sendWs conn 
+  putStrLn "> Subscribe? [y/n]"
+  i <- getLine
+  
+  unless (i /= "y") $ do
+    msg <- mkReqIO
+    sendTextData conn (encode msg)
+    sendWs conn
 
 -- | Receive data from websocket
 receiveWs :: WS.Connection -> IO ThreadId
@@ -47,3 +58,36 @@ receiveWs conn = do
 killThreads :: WS.Connection -> [ThreadId]-> IO ()
 killThreads conn ts = do
   forM_ ts $ (flip throwTo) ThreadKilled
+
+
+-- -------------------------------------------------------------------
+-- Makers 
+
+mkReq :: ServerRequest
+mkReq = ServerRequest 1 "SUBSCRIBE" ["btcusdt@miniTicker"]
+
+mkReq' :: Int -> Text -> Text -> ServerRequest
+mkReq' reqId method params = ServerRequest
+  { _srqRequestId = reqId
+  , _srqMethod    = method
+  , _srqParams    = [params] }
+
+mkReqIO :: IO ServerRequest
+mkReqIO = do
+  rid <- R.randomRIO (1, 999)
+  prompt "> Enter params: "
+  params <- getLine
+  return $ ServerRequest rid "SUBSCRIBE" [T.strip (T.pack params)]
+  where
+    prompt p = putStrLn p
+
+-- -------------------------------------------------------------------
+-- Main
+
+main :: IO ()
+main =
+  Wuss.runSecureClient url port path wsApp
+  where
+    url  = "stream.binance.com"
+    port = 9443
+    path = "/ws"
